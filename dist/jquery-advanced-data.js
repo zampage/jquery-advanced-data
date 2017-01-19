84727ddda4299f25dbb1f511ede66fb4e53be6c9
Markus Chiarot <cmarkus@hotmail.ch>
2017-01-19 23:13:29 +0100

    implement basic reading

--- dist/jquery-advanced-data.js
+++ dist/jquery-advanced-data.js
@@ -1,8 +1,73 @@
 /**
  * @author Markus Chiarot
  * @website https://github.com/zampage/jquery-advanced-data#readme 
- * @version 0.0.1
+ * @version 0.0.2
  * 
  * jQuery-advanced-data is a jQuery plugin for optimizing data attribute handling
  */
-/* hello world! */
+'use strict';
+
+(function ($) {
+
+    var ALL_DATA = 'jad-give-me-all-data-attributes';
+
+    /**
+     * get all attributes from a node
+     *
+     * @param node
+     * @returns {Array}
+     */
+    var getAllAttributes = function getAllAttributes(node) {
+        var attributes = [];
+        $.each(node.attributes, function () {
+            if (this.specified) {
+                attributes[this.name] = this.value;
+            }
+        });
+        return attributes;
+    };
+
+    /**
+     * distinguish what attributes to get and return them
+     *
+     * @param key
+     * @param node
+     * @param $node
+     * @returns {Array}
+     */
+    var getAttribute = function getAttribute(key, node, $node) {
+        return key == ALL_DATA ? getAllAttributes(node) : $node.attr('data-' + key);
+    };
+
+    /**
+     * main jquery-plugin function
+     *
+     * @param key
+     * @returns {Array}
+     */
+    $.fn.data = function (key) {
+
+        //parameters
+        key = key || ALL_DATA;
+
+        //init result
+        var result = [];
+
+        //loop nodes
+        this.each(function () {
+
+            //set current node
+            var node = this;
+            var $node = $(node);
+
+            //get data
+            var attribute = getAttribute(key, node, $node);
+
+            //add data to result
+            result.push([attribute, this]);
+        });
+
+        //return one or multiple results
+        return result.length == 1 ? result[0][0] : result;
+    };
+})(jQuery);
--- jquery-advanced-data.js
+++ jquery-advanced-data.js
@@ -1,1 +1,66 @@
-/* hello world! */
+(function($){
+
+    const ALL_DATA = 'jad-give-me-all-data-attributes';
+
+    /**
+     * get all attributes from a node
+     *
+     * @param node
+     * @returns {Array}
+     */
+    const getAllAttributes = function(node){
+        var attributes = [];
+        $.each(node.attributes, function(){
+            if(this.specified){
+                attributes[this.name] = this.value;
+            }
+        });
+        return attributes;
+    };
+
+    /**
+     * distinguish what attributes to get and return them
+     *
+     * @param key
+     * @param node
+     * @param $node
+     * @returns {Array}
+     */
+    const getAttribute = function(key, node, $node){
+        return (key == ALL_DATA) ? getAllAttributes(node) : $node.attr('data-' + key);
+    };
+
+    /**
+     * main jquery-plugin function
+     *
+     * @param key
+     * @returns {Array}
+     */
+    $.fn.data = function(key){
+
+        //parameters
+        key = key || ALL_DATA;
+
+        //init result
+        let result = [];
+
+        //loop nodes
+        this.each(function(){
+
+            //set current node
+            let node = this;
+            let $node = $(node);
+
+            //get data
+            let attribute = getAttribute(key, node, $node);
+
+            //add data to result
+            result.push([attribute, this]);
+
+        });
+
+        //return one or multiple results
+        return (result.length == 1) ? result[0][0] : result;
+    };
+
+}(jQuery));
